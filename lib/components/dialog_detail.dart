33c1886ab4c92fa12fc4cfc15b566081975348e2
hinata2145 <hinatamaru2145@gmail.com>
2023-11-26 13:52:07 +0900

    ポップアップ画面を作った。

--- lib/components/dialog_detail.dart
+++ lib/components/dialog_detail.dart
@@ -12,13 +12,72 @@ class _DialogDetailState extends State<DialogDetail> {
   Widget build(BuildContext context) {
     return AlertDialog(
       contentPadding: EdgeInsets.zero,
-      content: Column(
-        children: [
-          Image.network(
-            height: 300,
-            'https://www.mbs.jp/jujutsukaisen/images/head_230901.webp',
-          ),
-        ],
+      content: Container(
+        color: const Color(0xFF242E43),
+        height: 500,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  color: Colors.red,
+                  height: 280,
+                  width: 500,
+                  child: FittedBox(
+                    fit: BoxFit.fill,
+                    child: Image.network(
+                      'https://www.mbs.jp/jujutsukaisen/images/head_230901.webp',
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 1,
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.close,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  "呪術廻戦",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+                Text(
+                  "2023   2シーズン",
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text("あらすじ",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold)),
+                Text(
+                    "テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold))
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
