84fe7262cacc2df560ca7f5831b1e160e670f3a6
hinata2145 <hinatamaru2145@gmail.com>
2023-12-05 18:31:06 +0900

    ポップアップ画面にカテゴリ名を追加した。

--- lib/components/dialog/category_name.dart
+++ lib/components/dialog/category_name.dart
@@ -4,12 +4,16 @@ class CategoryName extends StatelessWidget {
   const CategoryName({Key? key}) : super(key: key);
 
   @override
-  State<> createState() => _MyCustomFormState();
-}
-
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+        height: 24,
+        color: Colors.orange,
+        child: const Padding(
+          padding: EdgeInsets.only(right: 10, left: 10),
+          child: Text(
+            "SF/ファンタジー",
+            style: TextStyle(color: Colors.white),
+          ),
+        ));
   }
 }
-
